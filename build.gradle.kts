e9b3f459fd169d6afa6acd80604fe170fe0427a3
OakLoaf <oak@beaconstudios.org>
2025-06-10 21:01:23 +0100

    Bumped version to 1.1.22

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ allprojects {
     apply(plugin = "com.gradleup.shadow")
 
     group = "org.lushplugins"
-    version = "1.1.21"
+    version = "1.1.22"
 
     repositories {
         mavenLocal()
